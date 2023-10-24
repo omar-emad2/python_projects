778a944f279ca9d1dad2e6442a65b7ae7f11dcd9
Omar Emad <148720194+omar-emad2@users.noreply.github.com>
2023-10-24 19:04:03 +0300

    Create python if condition.py

---
+++ python if condition.py
@@ -0,0 +1,40 @@
+age = 15
+
+if age >= 15:
+    print('you are a man')
+else:
+    print('go out you are a kid!')
+
+
+
+
+
+number=input('How old are you ? \n ')
+if int(number) >= 15:
+        print('you are man')
+else : 
+        print('you are kid')
+
+
+
+
+
+sis=input('Do you have siblings ? \n ')
+if str(sis)== 'yes':
+    print('you are donkey')
+elif sis=='i dont know':
+    print('you are dick')
+else :
+    print('fuck you')
+
+
+
+sis=input (' do you have sisters')
+if str(sis)== 'yes':
+  print('you are donkey')
+elif sis=='i dont know':
+  print('you are dick')
+elif sis=='no':
+  print('you typed no, So go to hell man!')
+else:
+  print('fuck you')
