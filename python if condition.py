2bc2929eaa7f0d14ad8c6966465e6f02f54ab738
Omar Emad <148720194+omar-emad2@users.noreply.github.com>
2023-10-24 19:24:33 +0300

    Update python if condition.py

--- python if condition.py
+++ python if condition.py
@@ -38,3 +38,15 @@ elif sis=='no':
   print('you typed no, So go to hell man!')
 else:
   print('fuck you')
+
+
+first_input = float(input("Enter the first number: "))
+second_input = float(input("Enter the second number: "))
+third_input = float(input("Enter the third number: "))
+
+if first_input >= second_input and first_input >= third_input:
+  print('The Largest number is from first ' + str(first_input))
+elif second_input >= first_input and second_input >= third_input:
+  print('The Largest number is from second ' + str(second_input))
+else:
+  print('The Largest number is from third ' + str(third_input))
