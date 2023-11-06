5af91f98dd89b0cfd56c95e0c8038d6a77966d85
Omar Emad <148720194+omar-emad2@users.noreply.github.com>
2023-11-06 20:36:46 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#A project to calculate the area of ​​the room and give the amount for the cost of the work
+length=input('Please type length :\n')
+width= input('Please type width :\n')
+# i need to change from string to float 
+float_length=float(length)
+float_width=float(width)
+money = input ('How much for 1 meter ? \n')
+float_money = float(money)
+area= (float_length*float_width*2)
+# i need to change from float to  string
+str_area=str(area)
+print('The total are is :'+str_area )
+budget=(float_money*area)
+str_budget=str(budget)
+print('Give the guy : $'+str_budget)
